b0ecea50482c71b3e35c434a890340bf31b14fa0
anbcodes <anbcodes@protonmail.com>
2020-11-19 16:02:30 -0500

    added more functions

--- builtins.js
+++ builtins.js
@@ -1,4 +1,12 @@
 //--create
 widget('compile', 'textarea', $main, (value) => {
-    return btoa(LZString.compress(value))
+    return LZString.compressToBase64(value)
+})
+//--getlink
+widget('get link', 'input', $main, (value) => {
+    return `https://widget.anb.codes/?i=${value}`
+})
+//--gethiddenlink
+widget('get hide link', 'input', $main, (value) => {
+    return `https://widget.anb.codes/?hide=true&i=${value}`
 })
